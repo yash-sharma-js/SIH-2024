cba4a4b2958e714843d1cbc45eb547b7b22bcff6
ayushduseja45 <dusejaayush2910@gmail.com>
2024-08-27 12:28:03 +0530

    History of no2 added

--- Client/frontend/src/App.jsx
+++ Client/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import StatBoxes1 from "./components/statistics/StatBoxes1.jsx"
 import Map from "./components/Map.jsx"
 import StatBoxes2 from "./components/statistics/StatBoxes2.jsx"
 import AQIHeatMap from "./components/Aqi_Heat_Map.jsx"
-
+import HistoryNO2Component from "./components/HistoryNO2.jsx"
 function App() {
 
 
@@ -13,6 +13,7 @@ function App() {
       <StatBoxes1/>
       <Map/>
       <StatBoxes2/>
+      <HistoryNO2Component/>
       <AQIHeatMap/>
     </div>
   )
